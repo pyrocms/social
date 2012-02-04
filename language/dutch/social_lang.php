8c01a6d33b02afb42fc39b42c746ed39571c6cda
Jaap Jolman <jaap@jolman.eu>
2012-02-04 20:55:11 +0100

    Added dutch language

---
+++ language/dutch/social_lang.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['social:client_key']                  = 'Client ID/Sleutel';
+$lang['social:client_secret']               = 'Client Geheim';
+$lang['social:scope']             			= 'Scope';
+
+$lang['social:access_token']                = 'Toegangs Token';
+$lang['social:secret']             			= 'Geheim';
+$lang['social:expires']             		= 'Verloopt';
+$lang['social:refresh_token']               = 'Vernieuw Token';
+
+// messages
+$lang['social:missing_credentials'] 		= 'Geef een Client ID / Sleutel en Client Geheim op.';
+$lang['social:failed_save_authentication'] 	= 'Fout bij het opslaan van de authenticatie token.';
+$lang['social:failed_save_credentials'] 	= 'Fout bij het opslaan van de referenties.';
+$lang['social:save_credentials'] 			= 'Referenties voor "%s" zijn opgeslagen.';
+$lang['social:removed_credentials'] 		= 'Referenties voor "%s" zijn verwijderd.';
+
+// frontend
+$lang['social:linked_accounts']            	= 'Gekoppelde accounts';
+$lang['social:get_tokens']           	 	= 'Verkrijg Tokens';
+
+$lang['social:pick_account']           	 	= 'Kies Account';
+$lang['social:pick_account_message']   	 	= 'U heeft de mogelijkheid om te posten naar meerdere accounts of profielen, hier kunt u kiezen bij welke u zich wil verifiëren.';
